4bf259b4f164aeecc8961507ffb84bf904d3eb26
Cleyton-sousa <cleyton.santos@b2bti.com>
2024-07-27 22:06:14 -0300

    chore: add doc and suport to bigInt type from database

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,24 @@ async function bootstrap() {
       transform:true
     }
   ))
-  await app.listen(process.env.PORT || 3000);
+
+  //@ts-ignore
+  BigInt.prototype.toJSON = function () {
+    const int = Number.parseInt(this.toString());
+    return int ?? this.toString();
+  };
+
+
+  const config = new DocumentBuilder()
+  .setTitle('Back end dogrupo 7')
+  .setDescription('Api usada pra consumo do front end do grupo 7')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
+  await app.listen(process.env.PORT || 3000, () => {
+    console.log(`server is running on http://localhost:${process.env.PORT || 3000}`)
+  });
 }
 bootstrap();
