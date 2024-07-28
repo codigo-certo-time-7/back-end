3312a715d203662d98213f15b3765742fb1170ae
Cleyton-sousa <cleyton.santos@b2bti.com>
2024-07-27 22:07:32 -0300

    fix: code already broken fixed

--- src/technologies/entities/technologies.entity.ts
+++ src/technologies/entities/technologies.entity.ts
@@ -1,6 +1,7 @@
-import { Technologies } from '@prisma/client';
+import { $Enums, Technologies } from '@prisma/client';
 
 export class TechnologiesEntity implements Technologies {
+  level: $Enums.Level;
   id: bigint;
   candidateId: bigint;
   name: string;
