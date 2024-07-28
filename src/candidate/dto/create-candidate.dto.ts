84624a749ca77451bddfba6299577289e4e5f2f2
Cleyton-sousa <cleyton.santos@b2bti.com>
2024-07-27 22:07:02 -0300

    chore: add docs

--- src/candidate/dto/create-candidate.dto.ts
+++ src/candidate/dto/create-candidate.dto.ts
@@ -1,18 +1,43 @@
 import { $Enums } from "@prisma/client";
 import { IsEmail, IsEnum, IsNotEmpty, IsNumberString, IsString, Length } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCandidateDto {
+    @ApiProperty({
+        description: 'Nome do candidato',
+        example: 'João Silva',
+        maxLength: 255
+    })
     @IsString()
     @IsNotEmpty()
-    @Length(3,255)
+    @Length(3, 255)
     name: string;
+
+    @ApiProperty({
+        description: 'Email do candidato',
+        example: 'joao.silva@example.com'
+    })
     @IsEmail()
     @IsNotEmpty()
     email: string;
+
+    @ApiProperty({
+        description: 'Telefone do candidato',
+        example: '11987654321',
+        minLength: 11,
+        maxLength: 11
+    })
     @IsNumberString()
     @IsNotEmpty()
-    @Length(11,11)
+    @Length(11, 11)
     phone: string;
+
+    @ApiProperty({
+        description: 'Tipo de contribuição do candidato',
+        enum: $Enums.Contribution,
+        example: $Enums.Contribution.APPRENTICE
+    })
     @IsEnum($Enums.Contribution)
-    contributionType: $Enums.Contribution
+    contributionType: $Enums.Contribution;
 }
+
